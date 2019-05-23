5966cf1b2173d19d070d0836545833e2ba4bdf8e
Eddie Atkinson <eddiebatkinson@gmail.com>
2019-05-23 10:15:47 -0400

    sounds and words only start once button is clicked

--- js/index.js
+++ js/index.js
@@ -1,38 +1,45 @@
-console.log('CONNECTED!');
 const upSound = new Audio('./assets/up.m4a');
 const downSound = new Audio('./assets/down.m4a');
 const rightSound = new Audio('./assets/right.m4a');
 const leftSound = new Audio('./assets/left.m4a');
 let oldX = 0;
 let oldY = 0;
-function mouseMoveMethod(e) {
-  const { pageX, pageY } = e;
-  const changeInX = pageX - oldX;
-  const changeInY = pageY - oldY;
-  const xDirection = changeInX > 0 ? 'right' : 'left';
-  const yDirection = changeInY > 0 ? 'down' : 'up';
-  const overallDirection = Math.abs(changeInX) > Math.abs(changeInY) ? xDirection : yDirection;
-  let soundToPlay;
-  switch(overallDirection) {
-    case 'right':
-      soundToPlay = rightSound;
-      break;
-    case 'left':
-      soundToPlay = leftSound;
-      break;
-    case 'up':
-      soundToPlay = upSound;
-      break;
-    case 'down':
-      soundToPlay = downSound;
-      break;
-    default:
-      return null;
-  }
-  const element = document.getElementById('direction');
-  element.innerHTML = overallDirection;
-  oldX = pageX;
-  oldY = pageY;
-  soundToPlay.play();
+let clicked = false;
+function buttonClicked() {
+  console.log('Clicked!');
+  clicked = true;
 }
-document.addEventListener('mousemove', mouseMoveMethod)
+function mouseMoveMethod(e) {
+  if(clicked){
+    const { pageX, pageY } = e;
+    const changeInX = pageX - oldX;
+    const changeInY = pageY - oldY;
+    const xDirection = changeInX > 0 ? 'right' : 'left';
+    const yDirection = changeInY > 0 ? 'down' : 'up';
+    const overallDirection = Math.abs(changeInX) > Math.abs(changeInY) ? xDirection : yDirection;
+    let soundToPlay;
+    switch(overallDirection) {
+      case 'right':
+        soundToPlay = rightSound;
+        break;
+      case 'left':
+        soundToPlay = leftSound;
+        break;
+      case 'up':
+        soundToPlay = upSound;
+        break;
+      case 'down':
+        soundToPlay = downSound;
+        break;
+      default:
+        return null;
+    }
+    const element = document.getElementById('direction');
+    element.innerHTML = overallDirection;
+    oldX = pageX;
+    oldY = pageY;
+    soundToPlay.play();
+  }
+}
+
+document.addEventListener('mousemove', mouseMoveMethod);
